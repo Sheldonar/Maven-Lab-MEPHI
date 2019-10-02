92f1e3ca49d077502236fb734cfb0a61f7452af9
Sheldonar <gfi.post@gmail.com>
2019-10-02 21:18:11 +0300

    fedor commit

--- src/main/java/MainServlet.java
+++ src/main/java/MainServlet.java
@@ -9,7 +9,7 @@ import java.util.*;
 import java.util.UUID;
 
 
-@WebServlet("")
+@WebServlet("") // my comment
 public class MainServlet extends HttpServlet {
     private Map<Integer, Set<String>> base;
     public Singleton single;
